4834e245636a2a0543a251b2242ec2c1d4cc0dc5
David Frank <bitinn@users.noreply.github.com>
2014-08-14 14:22:38 +0800

    allow user to disable logger
    logger = false will disable stdout.

--- tasks/canned.js
+++ tasks/canned.js
@@ -24,7 +24,7 @@ module.exports = function (grunt) {
 
     var cannedOptions = {
       cors: true,
-      logger: process.stdout,
+      logger: options.logger || process.stdout,
       cors_headers: options.cors_headers
     };
 
